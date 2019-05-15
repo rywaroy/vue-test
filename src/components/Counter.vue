b0c94e230a8116420d2c8f9f7abad30574bfdb6c
zhangzhihao <464485682@qq.com>
2019-05-15 15:16:00 +0800

    Feat: Counter compontent

---
+++ src/components/Counter.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="counter">
+    <p class="title">{{title}}</p>
+    <div class="increase" @click="increase()">加</div>
+    <p class="count">{{count}}</p>
+    <p class="type">{{count | numberType}}</p>
+    <p class="total">{{base}} + {{count}} = {{total}}</p>
+    <div class="decrease" @click="decrease()">减</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    num: {
+      type: Number,
+      default: 1,
+    },
+  },
+  data() {
+    return {
+      count: 0,
+      base: 10,
+    };
+  },
+  methods: {
+    increase() {
+      this.count += this.num;
+    },
+    decrease() {
+      this.count -= this.num;
+    },
+  },
+  computed: {
+    total() {
+      return this.count + this.base;
+    },
+  },
+  filters: {
+    numberType(number) {
+      if (number > 0) {
+        return '正数';
+      }
+      if (number < 0) {
+        return '负数';
+      }
+      return '';
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
