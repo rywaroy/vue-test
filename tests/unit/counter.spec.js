29a43d541f509a9ade35231740889739c2fefa0b
zhangzhihao <464485682@qq.com>
2019-05-16 16:57:20 +0800

    feat: 测试计数器增加减少功能

--- tests/unit/counter.spec.js
+++ tests/unit/counter.spec.js
@@ -11,4 +11,16 @@ describe('HelloWorld.vue', () => {
     });
     expect(wrapper.find('.title').text()).toBe(title);
   });
+
+  it('测试计数器增加', () => {
+    const wrapper = shallowMount(Counter);
+    wrapper.find('.increase').trigger('click');
+    expect(wrapper.find('.count').text()).toBe('1');
+  });
+
+  it('测试计数器减少', () => {
+    const wrapper = shallowMount(Counter);
+    wrapper.find('.decrease').trigger('click');
+    expect(wrapper.find('.count').text()).toBe('-1');
+  });
 });
